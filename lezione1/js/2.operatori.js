b6fb1220a0c0e128a0fe7e96133929d92cadd48b
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2025-05-15 22:50:20 +0200

    lezione1/js/2.operatori.js

---
+++ lezione1/js/2.operatori.js
@@ -0,0 +1,42 @@
+// Operatori e operazioni
+
+// Gli operatori sono simboli che ci permettono di eseguire operazioni sui dati.
+
+// Variabili
+let a = 10;
+let b = 20;
+
+// Operatori aritmetici
+// Gli operatori aritmetici sono utilizzati per eseguire operazioni matematiche sui numeri.
+let c = a + b; // Somma
+let d = a - b; // Sottrazione
+let e = a * b; // Moltiplicazione
+let f = a / b; // Divisione
+let g = a % b; // Modulo (resto della divisione)
+let h = a ** b; // Potenza (a elevato alla b)
+
+// Operatori di incremento e decremento
+// Gli operatori di incremento e decremento sono utilizzati per aumentare o diminuire il valore di una variabile di 1.
+let i = a++; // Incremento (post-incremento)
+let j = ++a; // Incremento (pre-incremento)
+
+// Operatori di confronto
+// Gli operatori di confronto sono utilizzati per confrontare due valori e restituiscono un valore booleano (true o false).
+let k = a--; // Decremento (post-decremento)
+let l = --a; // Decremento (pre-decremento)
+
+// Gli operatori di confronto sono utilizzati per confrontare due valori e restituiscono un valore booleano (true o false).
+let m = a == b; // Uguaglianza (valore)
+let n = a === b; // Uguaglianza (valore e tipo)
+let o = a != b; // Non uguaglianza (valore)
+let p = a !== b; // Non uguaglianza (valore e tipo)
+
+// Operatori logici
+// Gli operatori logici sono utilizzati per combinare condizioni booleane.
+let q = a < b; // Minore di
+let r = a <= b; // Minore o uguale a
+let s = a > b; // Maggiore di
+let t = a >= b; // Maggiore o uguale a
+let u = a && b; // E logico: se a e b sono true, restituisce true
+let v = a || b; // O logic:o: se a o b sono true, restituisce true
+let w = !a; // Negazione logica: se a è true, restituisce false; se a è false, restituisce true
