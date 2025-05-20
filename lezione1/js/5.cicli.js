01ba43aa82f655d3fba0019838403e4fdc801ce7
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2025-05-20 09:38:32 +0200

    lezione1/js/5.cicli.js

---
+++ lezione1/js/5.cicli.js
@@ -0,0 +1,28 @@
+// CICLI
+// 1. For loop
+// 2. While loop
+// 3. Do-while loop
+
+// Ciclo for
+for (let i = 0; i < 10; i++) {
+    console.log(i); // Stampa i valori da 0 a 9
+}
+// Nota: Si usa quando si conosce il numero di iterazioni da eseguire
+
+// Ciclo while
+let j = 0;
+// Finchè j è minore di 10
+while (j < 10) {
+    console.log(j); // Stampa i valori da 0 a 9
+    j++;
+}
+// Nota: Si usa quando non si conosce il numero di iterazioni da eseguire
+
+// Ciclo do-while
+let k = 0;
+// Fai/ Esegui il blocco di codice almeno una volta
+do {
+    console.log(k); // Stampa i valori da 0 a 9
+    k++;
+} while (k < 10); // Finchè j è minore di 10
+// Nota: Si usa quando si vuole eseguire il blocco di codice almeno una volta
