75d4e1a2b42730ce069896f34a52efb45cf202c3
Daniele <100372697+daniele-segreto@users.noreply.github.com>
2025-05-28 16:10:37 +0200

    lezione1/js/6.funzioni.js

---
+++ lezione1/js/6.funzioni.js
@@ -0,0 +1,19 @@
+// FUNZIONI
+
+// Funzione per salutare
+function saluta() {
+    console.log("Ciao!");
+}
+saluta(); // Stampa "Ciao!"
+
+// Funzione che somma due numeri
+function somma(a, b) {
+    return a + b;
+}
+console.log(somma(10, 20)); // Stampa 30
+
+// Funzione che moltiplica due numeri
+function moltiplica(a, b) {
+    return a * b;
+}
+console.log(moltiplica(10, 20)); // Stampa 200
